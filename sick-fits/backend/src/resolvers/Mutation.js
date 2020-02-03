11aa9238439b9168fcfd18a330adcfb90c18f562
everton denis <evertondenis@MacBook-Pro-de-everton.local>
2020-02-03 17:08:59 -0300

    create and add Item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -5,6 +5,22 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
